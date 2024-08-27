7ba4ac45ff261a76e0ceddc823b41fa116be1b46
hmuri <minju1326@ewhain.net>
2024-08-27 22:41:31 +0900

    project 목록 가져오기

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,42 @@
 import LeftSide from "@/components/projects/LeftSide";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+
+interface Project {
+  id: string;
+  title: string;
+  description: string;
+  deadline: string;
+}
 
 const Index = () => {
   const router = useRouter();
+  const cookies = new Cookies();
+
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_REACT_APP_API_BASE_URL}/projects`,
+          {
+            headers: {
+              Authorization: cookies.get("accessToken"), // Assuming the token is stored in cookies
+            },
+          }
+        );
+
+        setProjects(response.data); // Assuming `list` contains the project data
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchProjects();
+  }, []);
   return (
     <div className="flex w-full">
       <LeftSide />
@@ -16,7 +50,16 @@ const Index = () => {
           </div>
         </div>
         <div className="flex flex-row h-[calc(100%-96px)]">
-          <div className="w-[400px] h-full border-r border-[#465069]"></div>
+          <div className="w-[400px] h-full border-r border-[#465069]">
+            <ul>
+              {projects?.map((project) => (
+                <li key={project.id}>
+                  <h2>{project.title}</h2>
+                  <p>Deadline: {project.deadline}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div></div>
         </div>
       </div>
